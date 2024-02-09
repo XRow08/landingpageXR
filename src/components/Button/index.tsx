fa082b8cd0df6e97b224d232e6afd164e20523aa
XRow08 <murilloaugustoe081@gmail.com>
2024-02-09 16:37:34 -0300

    fix: link zap

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,6 @@
 export default function Button() {
   return (
-    <a href="https://w.app/XRTech" className="lg:w-[428px] lg:h-[86px] w-[328px] h-[74px]">
+    <a href="https://bit.ly/42BkFQ3" className="lg:w-[428px] lg:h-[86px] w-[328px] h-[74px]">
       <button className="bg-button w-full h-full rounded-xl lg:py-7 px-11 transition duration-300 ease-in-out button-neon">
         <span className="text-white font-helvetica font-bold text-sm xl:text-xl">
           QUERO TER 10X MAIS RESULTADO
